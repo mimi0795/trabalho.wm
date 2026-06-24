93b08b1a21e9e4ef598160d42cdc1bce2cdfdcec
Gfreitas02 <gfs61766628@gmail.com>
2026-06-23 23:49:04 -0300

    corrige import App

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -31,6 +31,6 @@ export default defineConfig({
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  // File types to support raw imports. Never add .css, .jsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
