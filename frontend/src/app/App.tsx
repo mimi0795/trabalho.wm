7b796424ad7d2d615d9ea35646404096adacd50c
Gfreitas02 <gfs61766628@gmail.com>
2026-06-23 23:29:19 -0300

    minhas alterações

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from "react";
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { AppProvider } from './store/AppContext';
 
 export default function App() {
+
+  useEffect(() => {
+    const API = import.meta.env.VITE_API_URL;
+
+    fetch(`${API}/api/products`)
+      .then(res => res.json())
+      .then(data => {
+        console.log("Produtos:", data);
+      })
+      .catch(err => {
+        console.log("Erro:", err);
+      });
+  }, []);
+
   return (
     <AppProvider>
       <RouterProvider router={router} />
